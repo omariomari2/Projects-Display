0f2c5d299b65a20736ef461d5c775be577f1f82a
Owusu Bright Omari <owusuomaribright@gmail.com>
2025-02-20 00:29:23 -0600

    updated responsiveness

--- assets/js/toggleSections.js
+++ assets/js/toggleSections.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toggleSectionsButton');
     const sections = document.querySelectorAll('.section-content');
+    const body = document.body;
+    const mainContainer = document.querySelector('.main-container');
 
     toggleButton.addEventListener('click', function() {
         sections.forEach(section => {
             if (section.style.display === 'none' || section.style.display === '') {
                 section.style.display = 'block';
+                body.style.overflowY = 'auto';
+                mainContainer.style.overflowY = 'auto';
             } else {
                 section.style.display = 'none';
+                body.style.overflowY = 'auto';
+                mainContainer.style.overflowY = 'auto';
             }
         });
     });
